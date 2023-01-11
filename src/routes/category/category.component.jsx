a45f23576b55091069cc75c75a841e64161c0786
Alexandra Karlsson <betruebefree@gmail.com>
2023-01-11 13:41:59 +0100

    Added redux presist to store data and added redux thunk to handle loadingstate and errorstate

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -3,11 +3,13 @@ import {CategoryContainer, CategoryTitle} from './category.styles'
 import {Fragment, useEffect, useState} from "react";
 import ProductCard from "../../components/product-card/product-card.component";
 import {useSelector} from "react-redux";
-import {selectCategoriesMap} from "../../store/categories/category.selector";
+import {selectCategoriesIsLoading, selectCategoriesMap} from "../../store/categories/category.selector";
+import Spinner from "../../components/spinner/spinner.component";
 
 const Category = () => {
     const {category} = useParams();
     const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectCategoriesIsLoading)
     const [products, setProducts] = useState(categoriesMap[category])
 
     useEffect(() => {
@@ -17,9 +19,12 @@ const Category = () => {
     return (
         <Fragment>
             <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
+            {isLoading ? <Spinner/>:
             <CategoryContainer>
                 {products && products.map(product => <ProductCard key={product.id} product={product}/>)}
             </CategoryContainer>
+
+            }
         </Fragment>
     )
 }
